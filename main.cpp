f9b7f89826a797e32100ed2ea7aab0f1fc26ea80
SWE Cron Robotics Vitaly Bezugly <v.bezugly@asbis.com>
2023-02-05 00:57:40 +0200

    add code

---
+++ main.cpp
@@ -0,0 +1,22 @@
+
+// C++ prgroam to generate Gaussian filter 
+#include <iostream> 
+
+using namespace std; 
+
+void press_any_key();
+
+int main()
+{
+    std::cout << "vscode-hello-template start ...\n" << std::endl;
+
+    press_any_key();
+
+    return 0;
+} 
+
+void press_any_key()
+{
+    std::cout << "Press, enter ...\n";
+    int c = getchar();
+}
